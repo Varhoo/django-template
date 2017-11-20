c116b1910fa77146fcf61380ec1526851b7020e4
Pavel Studenik <pstudeni@redhat.com>
2017-11-20 14:46:57 +0100

    own directory for static file and storage

--- Dockerfile
+++ Dockerfile
@@ -2,20 +2,20 @@ FROM python:2-alpine
 
 RUN apk update && \
     apk add py-virtualenv && \
-    virtualenv /data/.env
+    virtualenv /data/app/.env
 
-ADD requirements.txt /data/requirements.txt
+ADD requirements.txt /data/app/requirements.txt
 
 RUN apk update && \
     apk add gcc zlib-dev libjpeg-turbo-dev python-dev musl-dev  \
             linux-headers libffi-dev postgresql-dev gettext
-RUN source /data/.env/bin/activate && pip install -r /data/requirements.txt && \
-    rm -rf /root/.cache/ && apk del gcc python-dev linux-headers libffi-dev python-dev && \
-    mkdir -p /data/proj/storage
+RUN source /data/app/.env/bin/activate && pip install -r /data/app/requirements.txt && \
+    rm -rf /root/.cache/ && apk del gcc python-dev linux-headers libffi-dev && \
+    mkdir -p /data/{storage, static}
 
-ADD . /data/
-WORKDIR /data/
+ADD . /data/app/
+WORKDIR /data/app/
 
 EXPOSE 80
 
-CMD sh /data/bin/docker-entrypoint.sh
+CMD sh /data/app/bin/docker-entrypoint.sh
--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/bash
 
 PROJ="proj"
-
+HOME_PATH="/data/app"
 # docker run -d --name "postgresql" -h "postgresql" -p 5432:5432 -e POSTGRES_PASSWORD=<passwd> -v /var/lib/postgresql/data:/var/lib/postgresql/data:Z postgres
 
 mkdir -p /data/log/
-source /data/.env/bin/activate
-PRODUCTION=$( find /data/ -name production.py | grep "settings/production.py" )
-WSGI_FILE=$( find /data/ -name wsgi.py -not \( -path "*/.env/*" \) )
+source $HOME_PATH/.env/bin/activate
+PRODUCTION=$( find $HOME_PATH -name production.py | grep "settings/production.py" )
+WSGI_FILE=$( find $HOME_PATH -name wsgi.py -not \( -path "*/.env/*" \) )
 export DJANGO_SETTINGS_MODULE="$PROJ.settings"
 if [ ! -z $PRODUCTION ]; then
     export DJANGO_SETTINGS_MODULE="$PROJ.settings.production"
 fi
 
 # python /data/manage.py migrate --fake-initial --noinput || exit
-python /data/manage.py migrate --noinput || exit
-python /data/manage.py collectstatic --noinput
+python $HOME_PATH/manage.py migrate --noinput || exit
+python $HOME_PATH/manage.py collectstatic --noinput
 # python manage.py compilemessages
 
 uwsgi --http :80 --thunder-lock --enable-threads --master --wsgi-file $WSGI_FILE
