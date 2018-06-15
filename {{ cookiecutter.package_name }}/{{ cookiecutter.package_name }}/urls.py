e13e1d9d0fc48a95190ac9f07bf3d7981e74cb57
Pavel Studenik <studenik@varhoo.cz>
2018-06-15 09:45:54 +0200

    static files only for debug mode

--- {{ cookiecutter.package_name }}/{{ cookiecutter.package_name }}/urls.py
+++ {{ cookiecutter.package_name }}/{{ cookiecutter.package_name }}/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import include
+
 from django.conf.urls import url
 from django.contrib import admin
 from django.conf import settings
@@ -24,5 +24,8 @@ import apps.core.views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', apps.core.views.home, name="homepage"),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
-  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
