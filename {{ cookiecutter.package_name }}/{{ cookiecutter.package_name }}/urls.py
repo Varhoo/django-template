e5fe621d107a9756411b6f53d8fd07d5eb2f4232
Pavel Studeník <pavel.studenik@kiwi.com>
2020-02-24 14:40:38 +0100

    Migrate to Python 3 and Django 2

--- {{ cookiecutter.package_name }}/{{ cookiecutter.package_name }}/urls.py
+++ {{ cookiecutter.package_name }}/{{ cookiecutter.package_name }}/urls.py
@@ -16,13 +16,13 @@ Including another URLconf
 
 import apps.core.views
 from django.conf import settings
-from django.conf.urls import url
+from django.urls import path
 from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^$', apps.core.views.home, name="homepage"),
+    path('admin/', admin.site.urls),
+    path('', apps.core.views.home, name="homepage"),
 ]
 
 if settings.DEBUG:
