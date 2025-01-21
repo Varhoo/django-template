2257398f7050063e53e65f2b58d553082c1eda4b
Pavel Studenik <studenik@varhoo.cz>
2025-01-21 18:27:53 +0100

    chore: add production settiongs

---
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.package_name }}/settings/production.py
@@ -0,0 +1,8 @@
+from .basic import *  # pylint: disable=wildcard-import,unused-wildcard-import
+
+DEBUG = False
+
+ # Fill valid domain
+ALLOWED_HOSTS = ["*"] 
+# Fill url of project, example https://www.varhoo.cz 
+CSRF_TRUSTED_ORIGINS = ["*"]
